6998cd51bdf19b7c7e12c8ed5691a3dc63a0c0b7
xiaosansiji <xiaosansiji@gmail.com>
2022-04-24 19:03:04 +0800

    feat: add tdesign-vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <t-button>按钮</t-button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { Button as TButton } from 'tdesign-vue/es';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    TButton,
+  },
+  data() {
+    return {
+      current: 1,
+    };
+  },
 }
 </script>
 
